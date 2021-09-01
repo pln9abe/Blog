c5db7da68aaaf0952a8e62cddca9d07c7d9dee7f
Raymond <raymond@philippinedev.com>
2021-09-01 18:29:33 +0800

    added association of user to comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :comments
+
   def username
     return self.email.split('@')[0].capitalize
   end
