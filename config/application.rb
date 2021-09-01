92588d42d5414cdbf31a7479223ba7c51169251c
Raymond <raymond@philippinedev.com>
2021-09-01 21:16:10 +0800

    configure default timezone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module PracticePortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Singapore'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
