9bd6e009ba980d6761285679c4ef19d2564364b7
Raymond <raymond@philippinedev.com>
2021-09-09 15:56:48 +0800

    refactor

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true
   validates :body, presence: true
