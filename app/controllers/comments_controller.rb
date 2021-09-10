1b5fe2b95326af2e184df7998f3301f3a6854bdc
Raymond <raymond@philippinedev.com>
2021-09-10 15:33:47 +0800

    added delete for comments on blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,9 +7,19 @@ class CommentsController < ApplicationController
     redirect_to @commentable
   end
 
+  def destroy
+    @comment = @commentable.comments.find(params[:id])
+    if @comment.destroy
+      redirect_to @commentable
+    end
+  end
+
   private
+  def comment_params
+  end
 
   def comment_params
     params.require(:comment).permit(:body)
   end
+
 end
